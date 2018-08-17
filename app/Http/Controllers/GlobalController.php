15a16b7bb5076246373c27a7e068ced8b7bf28ab
Diego Vieira <diegovc10@hotmail.com>
2018-08-17 10:19:30 -0300

    Alterado limit e order trabalhos default

--- app/Http/Controllers/GlobalController.php
+++ app/Http/Controllers/GlobalController.php
@@ -29,7 +29,7 @@ class GlobalController extends Controller
             'a_z' => 'a - z'
         ];
 
-        $trabalhos = Trabalho::filtroStatus()->filtroCidade()->limit(4)->get();
+        $trabalhos = Trabalho::filtroStatus()->filtroCidade()->limit(20)->inRandomOrder()->get();
 
         return view('pagina-inicial', compact('filtro_ordem', 'trabalhos'));
     }
