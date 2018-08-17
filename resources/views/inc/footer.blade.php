12e7f3adff6cdc9c8103e04057f7053b5180f591
Diego Vieira <diegovc10@hotmail.com>
2018-08-17 20:09:16 -0300

    Tawkto

--- resources/views/inc/footer.blade.php
+++ resources/views/inc/footer.blade.php
@@ -138,6 +138,20 @@
             @endif
         </script>
 
+        <!--Start of Tawk.to Script-->
+        <script type="text/javascript">
+            var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+            (function(){
+                var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+                s1.async=true;
+                s1.src='https://embed.tawk.to/5b76ec18f31d0f771d83e6bb/default';
+                s1.charset='UTF-8';
+                s1.setAttribute('crossorigin','*');
+                s0.parentNode.insertBefore(s1,s0);
+            })();
+        </script>
+        <!--End of Tawk.to Script-->
+
         <script src="{{ mix('/js/global.js') }}"></script>
     </body>
 </html>
