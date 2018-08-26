da96bba668932baca8b34197841b3bdcc029fb97
Diego Vieira <diegovc10@hotmail.com>
2018-08-26 12:19:34 -0300

    Method mobile search

--- resources/assets/js/mobile-global.js
+++ resources/assets/js/mobile-global.js
@@ -573,7 +573,7 @@ $(document).ready(function() {
 
          $.ajax({
              url: $(this).attr('action'),
-             method: 'POST',
+             method: 'GET',
              dataType:'json',
              data: $(this).serialize(),
              success: function(data) {
