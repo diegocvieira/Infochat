b7670d4cba3b88156627414a6f8e5da078eb3788
Diego Vieira <diegovc10@hotmail.com>
2018-11-13 14:07:26 -0200

    Fixed about mobile

--- resources/views/mobile/about.blade.php
+++ resources/views/mobile/about.blade.php
@@ -12,27 +12,31 @@
             <div class="holder">
                 @for($i = 1; $i <= 5; $i++)
                     <div class="slide-wrapper banner{{ $i }}">
-                        <p>
-                            @if($i == 1)
-                                O infochat é o atendimento<br>online dos profissionais<br>e estabelecimentos<br>da sua cidade
-                            @elseif($i == 2)
-                                Você pode pedir<br>informações e tirar suas<br>dúvidas de um jeito rápido e fácil
-                            @elseif($i == 3)
-                                1. Encontre o estabelecimento ou<br>o profissional que você procura<br><br>2. Envie uma mensagem e<br>aguarde o atendimento
-                            @elseif($i == 4)
-                                Você também pode usar o<br>infochat no seu computador
-                            @else
-                                E ainda pode ativar seu perfil<br>de trabalho, para atender<br>seus clientes online<br><br><i>É totalmente grátis!</i>
+                        <div class="top">
+                            <p>
+                                @if($i == 1)
+                                    O infochat é o atendimento<br>online dos profissionais<br>e estabelecimentos<br>da sua cidade
+                                @elseif($i == 2)
+                                    Você pode pedir<br>informações e tirar suas<br>dúvidas de um jeito rápido e fácil
+                                @elseif($i == 3)
+                                    1. Encontre o estabelecimento ou<br>o profissional que você procura<br><br>2. Envie uma mensagem e<br>aguarde o atendimento
+                                @elseif($i == 4)
+                                    Você também pode usar o<br>infochat no seu computador
+                                @else
+                                    E ainda pode ativar seu perfil<br>de trabalho, para atender<br>seus clientes online<br><br><i>É totalmente grátis!</i>
+                                @endif
+                            </p>
+                        </div>
+
+                        <div class="bottom">
+                            <img src="{{ asset('img/about-mobile/' . $i . '.png') }}" alt="{{ $i }}º imagem do sobre nós" />
+
+                            @if($i == 4)
+                                <img class="note" src="{{ asset('img/about-mobile/note.png') }}" alt="{{ $i }}º imagem do sobre nós" />
                             @endif
-                        </p>
 
-                        <img src="{{ asset('img/about-mobile/' . $i . '.png') }}" alt="{{ $i }}º imagem do sobre nós" />
-
-                        @if($i == 4)
-                            <img class="note" src="{{ asset('img/about-mobile/note.png') }}" alt="{{ $i }}º imagem do sobre nós" />
-                        @endif
-
-                        <div class="deslizar"><span class="text">Deslize</span><span class="arrow right"></span></div>
+                            <div class="deslizar"><span class="text">Deslize</span><span class="arrow right"></span></div>
+                        </div>
                     </div>
                 @endfor
             </div>
